45480899f4c8d2448999101926666253c7ab6995
Scaniox <alexl.mills3@gmail.com>
2023-04-22 01:01:45 +0100

    schematic done, blocks located, uC1 + PWR routed

---
+++ firmware/uC1_firmware/src/main.cpp
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ *         File: headphones\firmware\uC1_firmware\src\main.cpp
+ *       Author: Alexander Mills (Scaniox#7130)
+ *      License: 
+ *         Date: 
+ *  Description: 
+ ******************************************************************************/
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
